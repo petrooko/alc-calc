4e579a02817174f838312e57a31be0a44f8f8775
petrooko <125383213+petrooko@users.noreply.github.com>
2025-11-27 19:56:01 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,7 @@
+const CACHE='alc-cache-v1';
+self.addEventListener('install',e=>{
+ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'])))
+});
+self.addEventListener('fetch',e=>{
+ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))
+});
